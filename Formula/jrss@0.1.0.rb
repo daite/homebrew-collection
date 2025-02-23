47384c135e5dc18ef0e57dae6422e5cc78bcc222
daite <blueskykind02@yahoo.co.jp>
2025-02-23 19:45:26 +0900

    updated

--- Formula/jrss@0.1.0.rb
+++ Formula/jrss@0.1.0.rb
@@ -1,8 +1,8 @@
 class Jrss < Formula
     desc "A tool to handle RSS feeds"
     homepage "https://github.com/daite/jrss"
-    url "https://github.com/daite/jrss/archive/refs/tags/v0.1.1.tar.gz"
-    sha256 "968a67fc27cd6900b4c8d078fbeba900d1ffc5c343a031a8dfdeecd4211c0790" # Replace with the actual SHA256 checksum
+    url "https://github.com/daite/jrss/archive/refs/tags/v0.1.0.tar.gz"
+    sha256 "c11373246191e9c1b782f5551b9af59f399cde3066a4aceb49b0b4de53ea3997" # Replace with the actual SHA256 checksum
     license "MIT"
   
     depends_on "go" => :build
@@ -11,4 +11,5 @@ class Jrss < Formula
       # Build the Go binary
       system "go", "build", "-o", bin/"jrss", "./cmd/jrss"
     end
+
 end  
